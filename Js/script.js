0a4952f87d7d39d4e3128781ecc97bb5443c03d5
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-06-12 17:04:16 +0000

    fixed errors

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 /* global confetti */
-"use strict"
+'use strict'
 
 // Copyright (c) 2025 Adrina Peighambarzadeh All rights reserved
 // Make sure rainSadEmojis is available (loaded from sadEmojiRain.js)
@@ -13,9 +13,9 @@
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register(
-    "/ICD2O-Unit-6-01-B-Adrina-peighambarzadeh/sw.js",
+    '/ICD2O-Unit-6-01-B-Adrina-peighambarzadeh/sw.js',
     {
-      scope: "/ICD2O-Unit-6-01-B-Adrina-peighambarzadeh/",
+      scope: '/ICD2O-Unit-6-01-B-Adrina-peighambarzadeh/'
     }
   )
 }
@@ -24,32 +24,32 @@ if (navigator.serviceWorker) {
  * This function simulates a Yes/No spinner.
  */
 // eslint-disable-next-line no-unused-vars
-function spin() {
-  const question = document.getElementById("question").value.trim()
-  const resultEl = document.getElementById("result")
-  const yesImage = document.getElementById("yesImage")
-  const noImage = document.getElementById("noImage")
+function spin () {
+  const question = document.getElementById('question').value.trim()
+  const resultEl = document.getElementById('result')
+  const yesImage = document.getElementById('yesImage')
+  const noImage = document.getElementById('noImage')
 
-  if (question === "") {
-    alert("Please enter a question.")
+  if (question === '') {
+    alert('Please enter a question.')
   } else {
-    let answer = Math.random() < 0.5 ? "Yes" : "No"
+    const answer = Math.random() < 0.5 ? 'Yes' : 'No'
 
-    resultEl.textContent = "Answer: " + answer
+    resultEl.textContent = 'Answer: ' + answer
 
-    if (answer === "Yes") {
-      yesImage.style.display = "block"
-      noImage.style.display = "none"
+    if (answer === 'Yes') {
+      yesImage.style.display = 'block'
+      noImage.style.display = 'none'
 
       // Confetti centered on my entire window
       confetti({
         particleCount: 150,
         spread: 100,
-        origin: { x: 0.5, y: 0.5 }, // center horizontally and vertically
+        origin: { x: 0.5, y: 0.5 } // center horizontally and vertically
       })
     } else {
-      yesImage.style.display = "none"
-      noImage.style.display = "block"
+      yesImage.style.display = 'none'
+      noImage.style.display = 'block'
     }
   }
 }
